479e793d67252b023af263b7da6f754215a8a550
AKHMAD FAUZAN <ozan6825@gmail.com>
2024-04-25 23:55:27 +0000

    init

---
+++ src/app/actions/remote.ts
@@ -0,0 +1,134 @@
+"use server";
+
+import axios, { AxiosProgressEvent, AxiosResponse } from "axios";
+import { v4 as uuidv4 } from "uuid";
+import qs from "qs";
+import FormData from "form-data";
+
+let jobs: { [id: string]: AxiosResponse<any, any> } = {};
+let status: { [id: string]: AxiosProgressEvent & { fileName: string } } = {};
+let results: { [id: string]: any } = {};
+
+export async function checkFile(url: string) {
+	const result = await axios.head(url);
+	return JSON.parse(JSON.stringify(result.headers));
+}
+
+export async function getStatus() {
+	return status;
+}
+
+export async function downloadFile(url: string, fileName: string) {
+	const id = uuidv4();
+	jobs[id] = await axios(url, {
+		responseType: "arraybuffer",
+		onDownloadProgress: (progressEvent: AxiosProgressEvent) => {
+			const progressWithFileName = progressEvent as AxiosProgressEvent & { fileName: string };
+			progressWithFileName.fileName = fileName;
+			status[id] = progressWithFileName;
+		},
+	});
+	results[id] = jobs[id].data;
+	return true;
+}
+
+export async function cancelDownload() {}
+
+export async function upload(id: string, fileName: string) {
+	const req1 = await axios(
+		"https://www.terabox.com/api/precreate?app_id=250528&web=1&channel=dubox&clienttype=0&jsToken=5FAB18A17E69904FE2E3BDC20BF030C1CCAC6A29803A4C13DFAB0953072F3A50E4B7CD758FD9EA83AD4F6B8A0B46EA9CAC210877009E0E94222A925F454BDA58&dp-logid=38607300137720150036",
+		{
+			method: "post",
+			headers: {
+				Accept: "application/json, text/plain, */*",
+				"Accept-Language": "en-US,en;q=0.9,id;q=0.8",
+				Connection: "keep-alive",
+				"Content-Type": "application/x-www-form-urlencoded",
+				Cookie: "csrfToken=hghng4NF9999756UFeyMSBmX; browserid=NdDg4pMyTllTl2ETJ3Bp4NmxEzOSMMh3oXh5vXM36tn_EXiubts8FHOxbJE=; __bid_n=18d07f83ede1bb246c4207; __stripe_mid=121f5556-fce9-4c12-a0f4-a022f6685d91d6a5af; lang=en; PANWEB=1; TSID=KSADeW5bLEKsjVU19rMXDZeAjAfaax4g; ndus=YTwUjKEteHuibMt_zgKLHMvgvvmoQWp4CyqCVdCa; ndut_fmt=FF327F2B8A5755F70959EE45BB5363991605B77C6F3A1E79FA7885C5138A3E2A",
+				Origin: "https://www.terabox.com",
+				Referer: "https://www.terabox.com/main?category=all",
+				"Sec-Fetch-Dest": "empty",
+				"Sec-Fetch-Mode": "cors",
+				"Sec-Fetch-Site": "same-origin",
+				"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0",
+				"X-Requested-With": "XMLHttpRequest",
+				"sec-ch-ua": '"Chromium";v="124", "Microsoft Edge";v="124", "Not-A.Brand";v="99"',
+				"sec-ch-ua-mobile": "?0",
+				"sec-ch-ua-platform": '"Windows"',
+			},
+			data: qs.stringify({
+				path: "/" + fileName,
+				autoinit: "1",
+				target_path: "/",
+				block_list: '["5910a591dd8fc18c32a8f3df4fdc1761"]',
+				local_mtime: "1713380413",
+			}),
+		}
+	);
+
+	const form = new FormData();
+	form.append("file", results[id], { filename: "blob" });
+
+	const req2 = await axios(
+		"https://c-jp.terabox.com/rest/2.0/pcs/superfile2?method=upload&app_id=250528&channel=dubox&clienttype=0&web=1&logid=MTcxNDA1MjIyNDA5MzAuNjg2ODg3NDE2NjMyMDk0OA==&path=" +
+			req1.data.path +
+			"&uploadid=" +
+			req1.data.uploadid +
+			"&uploadsign=0&partseq=0",
+		{
+			headers: {
+				Accept: "*/*",
+				"Accept-Language": "en-US,en;q=0.9,id;q=0.8",
+				Connection: "keep-alive",
+				"Content-Type": "multipart/form-data; boundary=----WebKitFormBoundaryYebnTtRBDSCsKh8M",
+				Cookie: "browserid=NdDg4pMyTllTl2ETJ3Bp4NmxEzOSMMh3oXh5vXM36tn_EXiubts8FHOxbJE=; __bid_n=18d07f83ede1bb246c4207; PANWEB=1; TSID=KSADeW5bLEKsjVU19rMXDZeAjAfaax4g; ndus=YTwUjKEteHuibMt_zgKLHMvgvvmoQWp4CyqCVdCa",
+				Origin: "https://www.terabox.com",
+				Referer: "https://www.terabox.com/",
+				"Sec-Fetch-Dest": "empty",
+				"Sec-Fetch-Mode": "cors",
+				"Sec-Fetch-Site": "same-site",
+				"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0",
+				"sec-ch-ua": '"Chromium";v="124", "Microsoft Edge";v="124", "Not-A.Brand";v="99"',
+				"sec-ch-ua-mobile": "?0",
+				"sec-ch-ua-platform": '"Windows"',
+				...form.getHeaders(),
+			},
+			data: form,
+			method: "POST",
+		}
+	);
+
+	const req3 = await axios(
+		"https://www.terabox.com/api/create?isdir=0&rtype=1&bdstoken=d19ceff098763ab16de685c4a8dc0eea&app_id=250528&web=1&channel=dubox&clienttype=0&jsToken=5FAB18A17E69904FE2E3BDC20BF030C1CCAC6A29803A4C13DFAB0953072F3A50E4B7CD758FD9EA83AD4F6B8A0B46EA9CAC210877009E0E94222A925F454BDA58&dp-logid=35353200295319470024",
+		{
+			headers: {
+				Accept: "application/json, text/plain, */*",
+				"Accept-Language": "en-US,en;q=0.9,id;q=0.8",
+				Connection: "keep-alive",
+				"Content-Type": "application/x-www-form-urlencoded",
+				Cookie: "csrfToken=hghng4NF9999756UFeyMSBmX; browserid=NdDg4pMyTllTl2ETJ3Bp4NmxEzOSMMh3oXh5vXM36tn_EXiubts8FHOxbJE=; __bid_n=18d07f83ede1bb246c4207; __stripe_mid=121f5556-fce9-4c12-a0f4-a022f6685d91d6a5af; lang=en; PANWEB=1; TSID=KSADeW5bLEKsjVU19rMXDZeAjAfaax4g; ndus=YTwUjKEteHuibMt_zgKLHMvgvvmoQWp4CyqCVdCa; ndut_fmt=78AC4970DA47F5E706E63FF96A67EE6AFC5BC470B86E8F007A48293ADAD72810",
+				Origin: "https://www.terabox.com",
+				Referer: "https://www.terabox.com/main?category=all",
+				"Sec-Fetch-Dest": "empty",
+				"Sec-Fetch-Mode": "cors",
+				"Sec-Fetch-Site": "same-origin",
+				"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0",
+				"X-Requested-With": "XMLHttpRequest",
+				"sec-ch-ua": '"Chromium";v="124", "Microsoft Edge";v="124", "Not-A.Brand";v="99"',
+				"sec-ch-ua-mobile": "?0",
+				"sec-ch-ua-platform": '"Windows"',
+			},
+			data: qs.stringify({
+				path: "/" + fileName,
+				size: Buffer.from(results[id]).byteLength,
+				uploadid: req2.data.uploadid,
+				target_path: "/",
+				block_list: '["' + req2.data.md5 + '"]',
+				local_mtime: "1714028576",
+			}),
+			method: "post",
+		}
+	);
+
+	return req3.data;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,149 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useState } from "react";
+import { checkFile, downloadFile, getStatus, upload } from "./actions/remote";
+import { AxiosProgressEvent } from "axios";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [input, setInput] = useState("");
+	const [status, setStatus] = useState<{ [id: string]: AxiosProgressEvent & { fileName: string } }>();
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	useEffect(() => {
+		async function i() {
+			try {
+				const res = await getStatus();
+				setStatus({ ...res });
+			} catch (error) {
+				console.log(error);
+			}
+			setTimeout(i, 600);
+		}
+		i();
+	}, []);
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+	return (
+		<>
+			<div className="bg-zinc-900 text-zinc-300">
+				<div className="container mx-auto py-8 min-h-screen flex flex-col">
+					<div className="flex flex-row">
+						<input type="text" placeholder="url" onChange={(e) => setInput(e.target.value)} className="max-w-md w-full outline-none px-2 h-10 bg-zinc-800 border border-zinc-700" />
+						<CheckUrlAndDownload input={input} />
+					</div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+					{/* <div className="mt-4">
+						<pre>{JSON.stringify(status, null, 4)}</pre>
+					</div> */}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+					<table className="table-auto mt-4 text-left">
+						<thead>
+							<tr>
+								<th className="h-10 border-y px-2">no</th>
+								<th className="h-10 border-y px-2">name</th>
+								<th className="h-10 border-y px-2">size</th>
+								<th className="h-10 border-y px-2">estimated</th>
+								<th className="h-10 border-y px-2">progress</th>
+								<th className="h-10 border-y px-2">rate</th>
+								<th className="h-10 border-y px-2">action</th>
+							</tr>
+						</thead>
+						<tbody>
+							{status ? (
+								Object.keys(status).map((v, i) => {
+									const data = status?.[v];
+									const isDownload = data.progress != 1;
+									return (
+										<tr key={i}>
+											<th className="h-10 border-b px-2">{i + 1}.</th>
+											<td className="h-10 border-b px-2">{data?.fileName}</td>
+											<td className="h-10 border-b px-2">{(data.total! / 1024 / 1024)?.toFixed(2)} MB</td>
+											<td className="h-10 border-b px-2">{data?.estimated?.toFixed(2)} s</td>
+											<td className="h-10 border-b px-2">{(data.progress! * 100)?.toFixed(2)} %</td>
+											<td className="h-10 border-b px-2">{(data.rate! / 1024 / 1024)?.toFixed(2)} MB/s</td>
+											<td className="h-10 border-b px-2">
+												<button onClick={() => upload(v, data.fileName)} disabled={isDownload} className={`${isDownload && "bg-green-800"} h-8 px-2 bg-green-600 text-white rounded-md`}>
+													upload
+												</button>
+											</td>
+										</tr>
+									);
+								})
+							) : (
+								<tr className="col-span-3">
+									<td className="text-center">No data available!</td>
+								</tr>
+							)}
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</>
+	);
+}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+function CheckUrlAndDownload({ input }: { input: string }) {
+	const [result, setResult] = useState<{ "content-length": string; "content-disposition": string }>();
+	const [filename, setFilename] = useState("");
+
+	var name = result?.["content-disposition"]?.match(/filename=(.+)/)?.[1];
+	name = name ? decodeURIComponent(name) : undefined;
+
+	async function check() {
+		try {
+			const res = await checkFile(input);
+			if (res) return setResult(res);
+		} catch (error) {
+			console.error(error);
+			alert("galat");
+		}
+	}
+
+	async function download() {
+		try {
+			setResult(undefined);
+			downloadFile(input, name || filename);
+		} catch (error) {
+			console.error(error);
+			alert("galat");
+		}
+	}
+
+	return (
+		<>
+			<button onClick={check} className="bg-zinc-800 border-zinc-700 border px-2 flex items-center justify-center ml-2">
+				CHECK
+			</button>
+
+			{result && (
+				<div className="absolute h-screen w-screen bg-zinc-900/70 top-0 left-0 flex justify-center items-center">
+					<div className="max-w-md w-full p-4 bg-zinc-900 border border-zinc-800 rounded-md flex flex-col">
+						<table className="font-mono">
+							<thead>
+								<tr>
+									<td>name</td>
+									<td className="px-1">:</td>
+									<td>
+										<input type="text" value={name || filename} onChange={(e) => setFilename(e.target.value)} className="bg-zinc-900 outline-none border border-zinc-800 text-zinc-300" />
+									</td>
+								</tr>
+								<tr>
+									<td>size</td>
+									<td className="px-1">:</td>
+									<td>{Math.round(+result["content-length"] / 1024 / 1024) + " MB"}</td>
+								</tr>
+							</thead>
+						</table>
+
+						<div className="flex flex-row gap-2 items-center">
+							<button onClick={() => setResult(undefined)} className="mt-4 font-mono w-fit ml-auto bg-red-700 rounded-md px-2 h-8 flex items-center justify-center">
+								cancel
+							</button>
+							<button onClick={download} className="mt-4 font-mono w-fit bg-green-700 rounded-md px-2 h-8 flex items-center justify-center">
+								download
+							</button>
+						</div>
+					</div>
+				</div>
+			)}
+		</>
+	);
 }
